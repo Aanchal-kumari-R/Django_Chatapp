8605d414b2a19fd24ffb78767f3acd0d84d692fb
Aanchal-kumari-R <aanchalkumari096@gmail.com>
2025-02-17 16:27:53 +0530

    Creating View for ChatRoom

--- chatapp/views.py
+++ chatapp/views.py
@@ -3,4 +3,9 @@ from .models import ChatRoom
 
 def index(request):  
     chatRooms = ChatRoom.objects.all()
-    return render(request,'chatapp/index.html',{'chatRooms':chatRooms})
+    return render(request,'chatapp/index.html',{'chatRooms':chatRooms})  
+
+def chatRoom(request,slug): 
+    chatRoom = ChatRoom.objects.get(slug=slug) 
+    return render(request,'chatapp/room.html',{'chatRoom' : chatRoom})  
+ 
