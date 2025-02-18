0c76fa0beeba780992fe6b9a8c28827e70d2ece5
Aanchal-kumari-R <aanchalkumari096@gmail.com>
2025-02-18 23:39:04 +0530

    Creating Routing

--- mysite/asgi.py
+++ mysite/asgi.py
@@ -9,8 +9,19 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 
 import os
 
-from django.core.asgi import get_asgi_application
+from django.core.asgi import get_asgi_application 
+from channels.auth import AuthMiddlewareStack  
+from channels.routing import ProtocolTypeRouter, URLRouter  
+import chatapp.routing 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({ 
+    'http':get_asgi_application(), 
+    'websocket':AuthMiddlewareStack( 
+        URLRouter( 
+            chatapp.routing.websocket_urlpatterns
+        )
+    ),  
+
+})
